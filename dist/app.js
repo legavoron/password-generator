765fbc5d4babd07bbd0157a4ad3fa4bfd3467f2c
Aleh Shchurko <alehshchurko@gmail.com>
2022-02-11 18:16:44 +0300

    start

---
+++ dist/app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var wrapper = document.createElement('div');
+wrapper.classList.add('wrapper');
+document.body.append(wrapper);
+
+var circleFonImage = document.createElement('div');
+circleFonImage.classList.add('circle_fon');
+wrapper.append(circleFonImage);
---
+++ src/app.js
@@ -0,0 +1,7 @@
+let wrapper = document.createElement('div');
+wrapper.classList.add('wrapper');
+document.body.append(wrapper);
+
+let circleFonImage = document.createElement('div');
+circleFonImage.classList.add('circle_fon');
+wrapper.append(circleFonImage);
